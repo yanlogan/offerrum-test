10f1dd669510f947424b4ccff04e01ebf975c547
Yan Logan <yanlogans@gmail.com>
2023-08-20 22:19:26 +0300

    feat: add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import Container from "../Container/Container";
+import Logo from "../Logo/Logo";
+import Button from "../Button/Button";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <Container>
+        <div className="Header__wrapper">
+          <Logo />
+          <nav className="Header__nav">
+            <ul>
+              <li className="Header__navItem">
+                <a href="/">Главная</a>
+              </li>
+              <li className="Header__navItem">
+                <a href="#info">Что даст обучение</a>
+              </li>
+            </ul>
+            <Button type="primary">Личный кабинет</Button>
+          </nav>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
