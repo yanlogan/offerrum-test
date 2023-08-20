81270a008c8c7075fac166725b82b0d38ad66983
Yan Logan <yanlogans@gmail.com>
2023-08-20 22:16:53 +0300

    feat: add Container

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import "./Container.scss";
+
+const Container = ({ children }) => {
+  return <div className="Container">{children}</div>;
+};
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Container;
