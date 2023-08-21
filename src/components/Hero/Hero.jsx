22dcbe9dc0faacba4be1e8210d191f21b4298897
Yan Logan <yanlogans@gmail.com>
2023-08-21 21:11:38 +0300

    feat: add Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,27 @@
-import Header from "../Header/Header";
+import Button from "../Button/Button";
+import Container from "../Container/Container";
+import HeroBg from "../HeroBg/HeroBg";
 import "./Hero.scss";
 
 const Hero = () => {
   return (
     <section className="Hero">
-      <Header />
+      <Container>
+        <div className="Hero__wrapper">
+          <HeroBg />
+          <div className="Hero__text">
+            <h1 className="Hero__title">
+              Не&nbsp;упусти возможность войти в&nbsp;
+              <strong>прибыльную нишу</strong>
+            </h1>
+            <p className="Hero__subtitle">
+              Получи все&nbsp;нужные навыки для&nbsp;заработка на&nbsp;NFT всего
+              за&nbsp;28&nbsp;дней!
+            </p>
+          </div>
+          <Button type="accent">Начать зарабатывать на&nbsp;NFT</Button>
+        </div>
+      </Container>
     </section>
   );
 };
