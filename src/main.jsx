1904e927516a34edfaa0df80015d011864d4a785
Yan Logan <yanlogans@gmail.com>
2023-08-21 15:43:32 +0300

    set --vh property

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.scss";
 
+const setVh = () => {
+  document.documentElement.style.setProperty(
+    "--vh",
+    `${0.01 * window.innerHeight}px`
+  );
+};
+
+setVh();
+
+["resize", "orientationchange"].forEach((e) =>
+  window.addEventListener(e, () => {
+    setVh();
+  })
+);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
